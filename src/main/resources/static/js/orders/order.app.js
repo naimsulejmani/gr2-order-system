3cf3c542c57499ad9673e71bccd3d2db89499f17
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-01-16 10:32:52 +0100

    a po ja bon nje commit qityneve

--- src/main/resources/static/js/orders/order.app.js
+++ src/main/resources/static/js/orders/order.app.js
@@ -15,6 +15,37 @@ class OrderApp {
             .value = new Date().toISOString().split("T")[0];
 
         this.onProductFormSubmit();
+        this.onProductSelectionChange();
+        this.onRealCalculation();
+        // this.productForm.querySelector("input#quantity")
+        //     .addEventListener('keyup', event => {
+        //         this.onRealCalculation(event)
+        //     });
+        this.productForm.querySelectorAll('input.real-calc').forEach(input=>{
+            input.addEventListener("keyup", event=> {
+                this.onRealCalculation(event)
+            })
+        })
+    }
+
+    onRealCalculation(event) {
+        const quantity = parseInt(this.productForm.querySelector("input#quantity").value, 10);
+        const price = parseFloat(this.productForm.querySelector("input#price").value);
+        const discount = parseFloat(this.productForm.querySelector("input#discount").value);
+        const total = quantity * price * (1 - discount / 100);
+        this.productForm.querySelector("input#total").value = `${total.toFixed(2)}`;
+    }
+
+    onProductSelectionChange() {
+        this.productForm.querySelector("select#productId")
+            .addEventListener("change", event => {
+                const productId = event.target.value;
+                const product = this.products.filter(p => p.id == productId)[0];
+                this.productForm.querySelector("input#price").value = product.price;
+                this.productForm.querySelector("input#quantity").focus();
+                this.productForm.querySelector("input#quantity").select();
+
+            });
     }
 
     async onProductFormSubmit() {
@@ -24,7 +55,7 @@ class OrderApp {
             const orderDto = Object.fromEntries(formData);
             console.log(orderDto);
 
-            const product = this.products.filter(p=>p.id==orderDto.productId)[0];
+            const product = this.products.filter(p => p.id == orderDto.productId)[0];
             console.log(product)
 
             this.productForm.querySelector("input#price").value = product.price;
@@ -40,9 +71,9 @@ class OrderApp {
     }
 
     async displayProducts() {
-        this.products  = await this.productApi.findAll();
+        this.products = await this.productApi.findAll();
         const selectElement = this.productForm.querySelector("select#productId");
-        this.products .forEach(product => {
+        this.products.forEach(product => {
             selectElement.innerHTML += `<option value="${product.id}">${product.name}</option>`;
         })
     }
